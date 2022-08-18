21dcab977e69f224f5f866915718468822c3bee1
beesan-ahmad <abumfarreh@gmail.com>
2022-08-18 13:19:09 +0300

    first angular task

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,31 @@
+import { Component,EventEmitter,Input,OnInit,Output } from "@angular/core";
+import { NgForm } from '@angular/forms';
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss']
+})
+export class UserComponent implements OnInit {
+ 
+  @Output() userADD = new EventEmitter<User>();
+  public NewUser:User={name:'',age:0,email:'',password:''}
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  addUser(form:NgForm){
+    if(!form.form.valid){
+      form.form.markAllAsTouched();
+    }
+    else
+    this.userADD.emit(this.NewUser);
+  }
+}
+export interface User{
+  name: string;
+  age: number;
+  email:string;
+  password:string;
+}
+
